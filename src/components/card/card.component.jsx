3fbe18572fb3a918a6c6ab2513df4452a95a6d6a
Markus Realica <markus.developer2@gmail.com>
2020-04-19 08:49:52 -0700

    Change card background-color based on pokemon's type

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './card.styles.css';
 
 export const Card = (props) => (
-    <div className='card-container'>
+    <div className={'card-container ' + props.pokemon.type[0].toLowerCase()+'-type'}>
         <img alt={props.pokemon.name + " image"} src={props.pokemon.img} />
         <h1> {props.pokemon.name} </h1>
     </div>
