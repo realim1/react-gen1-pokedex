58941864272888bed6eaa0c3b2662cb642a793ca
Markus Realica <markus.developer2@gmail.com>
2020-04-19 20:11:05 -0700

    Updates

---
+++ precache-manifest.1df2bebf53e566a8092795c45750bd1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6c91005ea38e4a1b7cfbde01124491",
+    "url": "/react-gen1-pokedex/index.html"
+  },
+  {
+    "revision": "e02114a26feddfd28903",
+    "url": "/react-gen1-pokedex/static/css/main.e598fc25.chunk.css"
+  },
+  {
+    "revision": "0cbfb3873fa7e805af72",
+    "url": "/react-gen1-pokedex/static/js/2.72fc3a72.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-gen1-pokedex/static/js/2.72fc3a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02114a26feddfd28903",
+    "url": "/react-gen1-pokedex/static/js/main.7ea86afd.chunk.js"
+  },
+  {
+    "revision": "71cf9ad558486e473002",
+    "url": "/react-gen1-pokedex/static/js/runtime-main.0f5b0e0c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e736607545e8c6e1a99e16a42456b3c9.js"
+  "/react-gen1-pokedex/precache-manifest.1df2bebf53e566a8092795c45750bd1c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gen1-pokedex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
