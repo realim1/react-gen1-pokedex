9d701919631cc5ce73148b12dfae0f5f4eb4b2e0
Markus Realica <markus.developer2@gmail.com>
2020-04-19 19:04:11 -0700

    Deploy

--- README.md
+++
@@ -1,7 +0,0 @@
-## Summary
-Simple React project that displays 1st generation Pokemon using data from https://github.com/realim1/pokemonGo-json.
-
-## Instructions
-1. Have Node installed
-2. Use command **npm install**
-3. Use command **npm start**
---
+++ precache-manifest.e736607545e8c6e1a99e16a42456b3c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebcf8c8316fc777fb703f7222246c15e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "455d1e07f714cae9f6e0",
+    "url": "/static/css/main.e598fc25.chunk.css"
+  },
+  {
+    "revision": "0cbfb3873fa7e805af72",
+    "url": "/static/js/2.72fc3a72.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.72fc3a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455d1e07f714cae9f6e0",
+    "url": "/static/js/main.5b3bb337.chunk.js"
+  },
+  {
+    "revision": "6f0bd7ca09d9ee76a465",
+    "url": "/static/js/runtime-main.54820e7c.js"
+  }
+]);
